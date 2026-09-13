ed567c6c5c90701a4a20b88e7b87c55d1897150c
Milo Hixson <mdhixson8@gmail.com>
2026-09-13 10:09:09 -0600

    code

--- helloWorld/Hello.java
+++ helloWorld/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Milo");
+        System.out.println("first script of code using java");
     }
 }
